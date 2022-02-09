eed06ab663f8baab91fd38e07c4657d752cfc386
Pengfei Wang <pengfei.wang@anker.com>
2022-02-09 17:37:43 +0800

    添加音视频学习路线

---
+++ 技术积累/音视频/路线/音视频学习路线.mm
@@ -0,0 +1,79 @@
+
+<map>
+  <node ID="root" TEXT="音视频学习路线">
+    <node TEXT="基础知识" ID="d4a290b644f7209e93cb91d8566c8f9f" STYLE="bubble" POSITION="right">
+      <node TEXT="C/C++" ID="cbb60579505fca3af175db4a8ea5d383" STYLE="fork">
+        <node TEXT="C 语言入门" ID="42c5152b978163c44b174b6833b702bd" STYLE="fork"/>
+        <node TEXT=" C++ 语言入门" ID="45d8782c3df1a27f661103a224fa8bd8" STYLE="fork"/>
+      </node>
+      <node TEXT="JNI" ID="42b328ad0bbcadbee4338bf1d3c5a103" STYLE="fork">
+        <node TEXT="JNI 从入门到掌握" ID="61dddbe831f56b88d37cd6abc71cd069" STYLE="fork"/>
+      </node>
+      <node TEXT="库" ID="d724072da7048926c5862b8cdadde076" STYLE="fork">
+        <node TEXT="交叉编译动态库、静态库的入门学习" ID="5157beb914279d8dca50f4c36d6b5c48" STYLE="fork"/>
+        <node TEXT="Shell 脚本入门" ID="eaf2259195ac28c940d2041364f87cce" STYLE="fork"/>
+        <node TEXT="FFmpeg 4.2.2 交叉编译" ID="2a2608c2bd0c3ab13b9a33651d7f5266" STYLE="fork"/>
+      </node>
+      <node TEXT="音视频" ID="d96bae908fd1de8718e633c62b632c69" STYLE="fork">
+        <node TEXT="雷神-视音频编解码技术零基础学习方法" ID="c147c4160469ca672c80c94d412f15d0" STYLE="fork"/>
+        <node TEXT="雷神-视音频数据处理入门：RGB、YUV像素数据处理" ID="ae319c1e1bdf71ca962afd062d4fa059" STYLE="fork"/>
+        <node TEXT="雷神-视音频数据处理入门：PCM音频采样数据处理" ID="7c2acdef484e44d2ffaf0ce923afcfae" STYLE="fork"/>
+        <node TEXT="雷神-视音频数据处理入门：H.264视频码流解析" ID="faca49b01aed58b60b4ebf8f4025e5f1" STYLE="fork"/>
+        <node TEXT="雷神-视音频数据处理入门：AAC音频码流解析" ID="243f428d1f42b8f565b822edebb2ee56" STYLE="fork"/>
+        <node TEXT="音视频学习 (七) 掌握音频基础知识并使用 AudioTrack、OpenSL ES 渲染 PCM 数据" ID="e85eb2653e88f5249d8ad30aacb1291c" STYLE="fork"/>
+        <node TEXT="音视频学习 (八) 掌握视频基础知识并使用 OpenGL ES 2.0 渲染 YUV 数据" ID="7dc8c8e9facf5f3197ecaedc42b2cdbf" STYLE="fork"/>
+      </node>
+      <node TEXT="软硬编解码" ID="a44c85ab4ea2b48d7412650631ec378d" STYLE="fork">
+        <node TEXT="Libfdkaac 音频编解码" ID="1b19fea4845311346e044c3da2c21ecd" STYLE="fork"/>
+        <node TEXT="Libx264 视频编码" ID="e1a971c5776f98e991603189dc159cf8" STYLE="fork"/>
+        <node TEXT="Android MediaCodec AAC 硬编解码" ID="952f80a2d0410d0bf70103ccd00b9747" STYLE="fork"/>
+        <node TEXT="Android MediaCodec H264 硬编解码" ID="fdf82a4f4642fbbe051d023855d98a39" STYLE="fork"/>
+        <node TEXT="FFmpeg API 实现音频 AAC 软编解码" ID="1012b9271651e6d7ac7be9822ab8107c" STYLE="fork"/>
+        <node TEXT="FFmpeg API 实现视频 H264 软编解码" ID="878832f6b6224a357901b6cf059454c0" STYLE="fork"/>
+        <node TEXT="MP3 编解码" ID="5718b1972bc00385ef12c3604a5a28d0" STYLE="fork"/>
+        <node TEXT="ffmpeg 基于 h264,AAC 文件打包为 MP4" ID="aec88df51fd18cdb1bd2d416843f6aee" STYLE="fork"/>
+        <node TEXT="Java API 实现音视频播放(mp3/mp4/pcm/yuv)" ID="e072909b92e213c3dfc04ca5ce1ccf5b" STYLE="fork"/>
+        <node TEXT="Native 端实现音视频播放(PCM/YUV)" ID="2a8831b760dac26c04f208e30bf58725" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="中级知识" ID="081fe92d6d6e820674e71c001cd23aaa" STYLE="bubble" POSITION="right">
+      <node TEXT="FFmpeg + OpenGL ES + OpenSL ES + soundtouch 完成音视频播放" ID="50c634c181f92524a69845b4d832e94f" STYLE="fork">
+        <node TEXT="Google 官方的音视频播放库 -grafika" ID="3ec2bf01053cc09f105020312ccd5893" STYLE="fork"/>
+        <node TEXT="bilibili 官方开源的音视频播放库- ijkplayer" ID="2b83a0d52f8801716cee9e4432396bc4" STYLE="fork"/>
+        <node TEXT="AVEditor 音视频播放模块---个人练习" ID="d0495792442268605c291d768d03a8a6" STYLE="fork"/>
+      </node>
+      <node TEXT="OpenGL ES 渲染视频" ID="35f463b2eeb21b023d3ae7335759766e" STYLE="fork">
+        <node TEXT="NDK_OpenGLES_3_0" ID="241d6a4f07b31d93bf104806951023e7" STYLE="fork"/>
+      </node>
+      <node TEXT="Camera + MediaCodec + OpenGL ES + OpenSL ES + FFmpeg MP4Muxer 实现音视频录制为 MP4 格式" ID="9e8c682b407b3c18f47fcc7030dca587" STYLE="fork">
+        <node TEXT="camera_recorder" ID="a6e52baf93d09e8342a9dc9f07c51b4d" STYLE="fork"/>
+      </node>
+      <node TEXT="RTMP 推流实现" ID="61384f04c118dfea05cb45658bbf08f0" STYLE="fork">
+        <node TEXT="AVRtmpPushSDK" ID="daacfd5155de9c0decdd7fbb3a82a1cd" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="高级知识" ID="ceb3deece1b19343c5c4d8ac1222e1b2" STYLE="bubble" POSITION="right">
+      <node TEXT="音视频变速、变调录制" ID="8d5ed2e6f9b5273ca1101c4b713687c3" STYLE="fork">
+        <node TEXT="音频-soundtouch" ID="bc37be9f3db5aabf10af5535d86caaf7" STYLE="fork"/>
+        <node TEXT="视频修改时间戳即可达到变速录制" ID="9e4442c32defd6ae57acfa009c952658" STYLE="fork"/>
+      </node>
+      <node TEXT="音视频录制实时滤镜实现" ID="4d68a546d7ac1dd229db4ab5061275f1" STYLE="fork">
+        <node TEXT="android-gpuimage" ID="0183b68cf172c7afe26121f08127ae83" STYLE="fork"/>
+      </node>
+      <node TEXT="分段录制、水印、背景音" ID="53e56feecde2326d975fcee127a4104f" STYLE="fork"/>
+      <node TEXT="人脸识别-特效" ID="bc939b45d081258c80821bacffd04fb7" STYLE="fork">
+        <node TEXT="OpenCV " ID="cdab048e546887abab025d53d108f14d" STYLE="fork"/>
+        <node TEXT="OpenGL " ID="9bbc7db8a08e11138c46500a7706f680" STYLE="fork"/>
+      </node>
+      <node TEXT="音视频剪辑技术" ID="a03882adb477358a543d4d677a0e3b89" STYLE="fork"/>
+    </node>
+    <node TEXT="书籍" ID="bac731f31a3db9e86b4aa7b5820fd596" STYLE="bubble" POSITION="right">
+      <node TEXT="音视频进阶指南" ID="beee4107b83d6f3b5ce2843cd08e7932" STYLE="fork"/>
+      <node TEXT="OpenGL ES 3.0 编程指南" ID="9cef81e808a6c8a8e3bf9ffa5dbf6d34" STYLE="fork"/>
+      <node TEXT="学习 OpenCV 3 中文版" ID="6c2efa13702a19f22aa1d65890cc1ae0" STYLE="fork"/>
+    </node>
+    <node TEXT="项目" ID="0200b0534f7b7fe8680842a94eb0a3c9" STYLE="bubble" POSITION="right">
+      <node TEXT="https://github.com/yangkun19921001/AVSample" ID="f20183b327ccd811a5613a27c67a45a3" STYLE="fork"/>
+    </node>
+  </node>
+</map>
